9a1c020be68c7c116b3b923738fa7470c462f321
Tania Allard <t.allard@sheffield.ac.uk>
2018-03-07 12:59:25 +0000

    Init

--- reveal.js/js/reveal-init.js
+++ reveal.js/js/reveal-init.js
@@ -17,11 +17,11 @@ Reveal.initialize({
     // },
 
     dependencies: [
-        { src: 'reveal.js/plugin/markdown/marked.js' },
-        { src: 'reveal.js/plugin/markdown/markdown.js' },
-        { src: 'reveal.js/plugin/notes/notes.js', async: true },
+        { src: './reveal.js/plugin/markdown/marked.js' },
+        { src: './reveal.js/plugin/markdown/markdown.js' },
+        { src: './reveal.js/plugin/notes/notes.js', async: true },
         {
-            src: '/reveal.js/plugin/highlight/highlight.js',
+            src: './reveal.js/plugin/highlight/highlight.js',
             async: true,
             condition: function () {
                 // Trim whitespace from code blocks, and prevent HTML escaping by default.
